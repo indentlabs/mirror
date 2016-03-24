c7939c7be8a1c185b02683c3d0b877e9e058626a
Jai Lalawat <jailalawat@gmail.com>
2016-03-24 14:38:58 +0530

    add permission

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,8 +239,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['FB_APP_ID'], ENV["FB_SECRET_KEY"]#, scope: ' user_posts, public_profile', info_fields: 'user_posts'
-
+  # config.omniauth :facebook, ENV['FB_APP_ID'], ENV["FB_SECRET_KEY"]#, scope: ' user_posts, public_profile', info_fields: 'user_posts'
+  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_SECRET_KEY'],:scope => 'email,user_posts', :setup => true
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
