96532fe0568163452fb72eb674d1f740a9dcc033
Jai Lalawat <jailalawat@gmail.com>
2016-03-26 12:25:55 +0530

    comment the get post code from thank you page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,17 +4,18 @@ class WelcomeController < ApplicationController
 	end
 
 	def thank_you
-		@posts = KoalaIntialize.get_posts(current_user.identity.access_token)
-		@posts.each do |post|
-			unless Post.find_by_post_id(post["id"]).present?
-				if post["message"].present?
-					user_post = current_user.posts.new(:post_id => post["id"], :message => post["message"])
-					if user_post.save
-						User.send_post(user_post.message,current_user.user_name)
-					end
-				end
-			end
-		end
+		@posts = current_user.posts
+		# @posts = KoalaIntialize.get_posts(current_user.identity.access_token)
+		# @posts.each do |post|
+		# 	unless Post.find_by_post_id(post["id"]).present?
+		# 		if post["message"].present?
+		# 			user_post = current_user.posts.new(:post_id => post["id"], :message => post["message"])
+		# 			if user_post.save
+		# 				User.send_post(user_post.message,current_user.user_name)
+		# 			end
+		# 		end
+		# 	end
+		# end
 	end
 
 	def privacy
